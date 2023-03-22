c98ac6f36429e14b9b96058ba473b9bfb913ab6c
Collins Kiragu <collins.kiragu@student.moringaschool.com>
2023-03-22 11:03:42 +0300

    Done

---
+++ db/migrate/20230321204641_create_students.rb
@@ -0,0 +1,12 @@
+class CreateStudents < ActiveRecord::Migration[6.1]
+  def change
+    create_table :students do |t|
+      t.references :instructor, null: false, foreign_key: true
+      t.string :name
+      t.integer :age
+      t.string :major
+
+      t.timestamps
+    end
+  end
+end
